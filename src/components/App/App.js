045496bc61778299a6b63e57871e8bde19792c90
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2024-01-09 09:39:43 -0800

    Build out prospective page layout in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import './App.css';
+import Playlist from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Spotify from '../util/Spotify';
 
 function App() {
   return (
     <div>
       <h1>
-        Sco<span></span>ttify
+        Sco<span className="highlight">TTT</span>ify
       </h1>
+      <div className="App">
+        <SearchBar onSearch={search} />
+        <div className="App-playlist">
+          <SearchResults searchResults={searchResults} onAdd={addTrack} />
+          <Playlist
+            playlistName={playlistName}
+            playlistTracks={playlistTracks}
+            onNameChange={updatePlaylistName}
+            onRemove={removeTrack}
+            onSave={savePlaylist}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
