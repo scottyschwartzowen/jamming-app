596a25a71344d644becd30918ee9add790211578
Scott Schwartz-Owen <scottyschwartzowen@gmail.com>
2024-01-09 10:20:22 -0800

    Build and style the search results component

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import styles from './Playlist.module.css'
+import React, { useCallback } from 'react';
 
-function Playlist() {
+import './Playlist.module.css';
+
+import TrackList from '../TrackList/TrackList';
+
+const Playlist = (props) => {
+  const handleNameChange = useCallback(
+    (event) => {
+      props.onNameChange(event.target.value);
+    },
+    [props.onNameChange]
+  );
+
   return (
-    <div>Playlist</div>
-  )
-}
+    <div className="Playlist">
+      <input onChange={handleNameChange} defaultValue={'New Playlist'} />
+      <TrackList
+        tracks={props.playlistTracks}
+        isRemoval={true}
+        onRemove={props.onRemove}
+      />
+      <button className="Playlist-save" onClick={props.onSave}>
+        SAVE TO SPOTIFY
+      </button>
+    </div>
+  );
+};
 
 export default Playlist;
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import styles from './SearchResults.module.css';
 
-function SearchResults() {
+import './SearchResults.module.css';
+
+import TrackList from '../TrackList/TrackList';
+
+const SearchResults = (props) => {
   return (
-    <div>SearchResults</div>
-  )
-}
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList tracks={props.searchResults} onAdd={props.onAdd} />
+    </div>
+  );
+};
 
 export default SearchResults;
